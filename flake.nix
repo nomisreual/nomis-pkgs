d1ff8cd0e76991a367277afe810595dbd0a77365
Simon Antonius Lauer <simon.lauer@posteo.de>
2024-07-15 22:57:56 +0200

    chore: initial commit

---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+  description = "Nomis PKGS";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }: 
+  let
+    pkgs = nixpkgs.legacyPackages.${system};
+    system = "x86_64-linux";
+  in
+  {
+    packages.${system}.python311 = pkgs.writeShellScriptBin "python" ''
+      export LD_LIBRARY_PATH=$NIX_LD_LIBRARY_PATH
+      exec ${pkgs.python311}/bin/python "$@"
+    '';
+  };
+}
